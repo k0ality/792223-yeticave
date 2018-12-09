bd6cde099b73600b753aaaca56b190ae1894b1e3
Maryna <marvlarz@gmail.com>
2018-12-09 15:06:25 +0200

    connect to MySQL from main scsript, run queries to get content from DB and use data for output in html-templates

--- index.php
+++ index.php
@@ -6,45 +6,58 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Снежок'; // укажите здесь ваше имя
 $user_avatar = 'img/user.jpg';
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-$lots = [
-    [
-        'title' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 10999,
-        'photo' => 'img/lot-1.jpg'
-    ],
-    [
-        'title' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 159999,
-        'photo' => 'img/lot-2.jpg'
-    ],
-    [
-        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => 8000,
-        'photo' => 'img/lot-3.jpg'
-    ],
-    [
-        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => 10999,
-        'photo' => 'img/lot-4.jpg'
-    ],
-    [
-        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => 7500,
-        'photo' => 'img/lot-5.jpg'
-    ],
-    [
-        'title' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => 5400,
-        'photo' => 'img/lot-6.jpg'
-    ],
+
+$db = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'A7YWh8$#',
+    'database' => 'yeticave'
 ];
+
+$con = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+
+mysqli_set_charset($con, "utf8");
+
+if (!$con) {
+    $error = mysqli_connect_error();
+    $page_content = include_template('pages/404.php', [
+        'error' => $error
+    ]);
+} else {
+    $categories_query = 'SELECT name FROM categories;';
+    $result_categories = mysqli_query($con, $categories_query);
+
+    if ($result_categories) {
+        $categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);
+    } else {
+        $error = mysqli_error($con);
+        $page_content = include_template('pages/404.php', [
+            'error' => $error
+        ]);
+    }
+
+    $lots_query = 'SELECT
+    lots.product,
+    lots.opening_price,
+    lots.image,
+    categories.name
+    FROM
+    lots
+    INNER JOIN categories ON categories.id = lots.category_id
+    WHERE
+    lots.closing_time > CURRENT_TIMESTAMP()
+    ORDER BY
+    lots.start_time DESC;';
+
+    if ($result_lots = mysqli_query($con, $lots_query)) {
+        $lots = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
+    } else {
+        $error = mysqli_error($con);
+        $page_content = include_template('pages/404.php', [
+            'error' => $error
+        ]);
+    }
+}
 
 $page_content = include_template(
     'index.php',
