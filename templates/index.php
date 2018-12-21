5caf6e4d4668c6f178a15e1865305c0485b2290f
Maryna <marvlarz@gmail.com>
2018-12-21 22:39:58 +0200

    fix promo list css styles

--- templates/index.php
+++ templates/index.php
@@ -5,7 +5,7 @@
                 горнолыжное снаряжение.</p>
             <ul class="promo__list">
                 <?php foreach ($categories as $category) : ?>
-                    <li class="promo__item promo__item--boards">
+                    <li class="promo__item promo__item--<?= $category['alias']; ?>">
                         <a class="promo__link" href="pages/all-lots.html"><?= $category['name']; ?></a>
                     </li>
                 <?php endforeach; ?>
