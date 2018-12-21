884809367591c50d838d1092d4dca6ebc08fc1ab
Maryna <marvlarz@gmail.com>
2018-12-21 19:07:36 +0200

    change all form validators to return null if successfull

--- functions/validators.php
+++ functions/validators.php
@@ -44,7 +44,7 @@ function validate_lot_form($post, $files)
         return $errors;
     }
 
-    return true;
+    return null;
 }
 
 function validate_sign_up_form($post, $files, $connection)
@@ -77,7 +77,7 @@ function validate_sign_up_form($post, $files, $connection)
         return $errors;
     }
 
-    return true;
+    return null;
 }
 
 function validate_login_form($post, $connection)
@@ -93,19 +93,11 @@ function validate_login_form($post, $connection)
         $errors['email'] = NONEXISTENT_EMAIL;
     }
 
-    if (filter_var($post['email'], FILTER_VALIDATE_EMAIL) && !(check_email_exist_in_db($connection, $post['email']))) {
-        $user = get_user_by_email($connection, $post['email']);
-
-        if (!password_verify($post['password'], $user['password'])) {
-            $errors['password'] = WRONG_PASSWORD;
-        }
-    }
-
     if (count($errors)) {
         return $errors;
     }
 
-    return true;
+    return null;
 }
 
 function notify_required_fields($input, $required)
